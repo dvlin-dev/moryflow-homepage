b4b01b24fd3e426dd852a835c1f4b6827ae9c2c2
zhangbaolin <zhangbaolin.work@foxmail.com>
2025-04-30 17:48:04 +0800

    feat: add umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,6 +25,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src="https://umami.dvlin.com/script.js"
+          data-website-id="68b9f47b-3f9c-47d5-96af-8dbcc12dba75"
+          defer
+        />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
